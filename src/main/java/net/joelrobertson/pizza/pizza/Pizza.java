460e930d058ecfae84a17a37ba513766dea1bd49
Joel Robertson <joel@joelrobertson.net>
2022-03-04 00:27:11 +1300

    add pizza base size and pizza topping to pizza entity

--- src/main/java/net/joelrobertson/pizza/pizza/Pizza.java
+++ src/main/java/net/joelrobertson/pizza/pizza/Pizza.java
@@ -2,6 +2,8 @@ package net.joelrobertson.pizza.pizza;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import net.joelrobertson.pizza.customerorder.CustomerOrder;
+import net.joelrobertson.pizza.pizzabasesize.PizzaBaseSize;
+import net.joelrobertson.pizza.pizzatopping.PizzaTopping;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import javax.persistence.*;
@@ -14,6 +16,14 @@ public class Pizza extends AbstractPersistable<Long> {
     @ManyToOne
     @JsonBackReference
     private CustomerOrder customerOrder;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(nullable = false)
+    private PizzaBaseSize pizzaBaseSize;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(nullable = false)
+    private PizzaTopping pizzaTopping;
 
     public String getName() {
         return name;
@@ -29,6 +39,22 @@ public class Pizza extends AbstractPersistable<Long> {
 
     public void setCustomerOrder(CustomerOrder customerOrder) {
         this.customerOrder = customerOrder;
+    }
+
+    public PizzaBaseSize getPizzaBaseSize() {
+        return pizzaBaseSize;
+    }
+
+    public void setPizzaBaseSize(PizzaBaseSize pizzaBaseSize) {
+        this.pizzaBaseSize = pizzaBaseSize;
+    }
+
+    public PizzaTopping getPizzaTopping() {
+        return pizzaTopping;
+    }
+
+    public void setPizzaTopping(PizzaTopping pizzaTopping) {
+        this.pizzaTopping = pizzaTopping;
     }
 
 }
