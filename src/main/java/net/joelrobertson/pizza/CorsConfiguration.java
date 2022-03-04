4f8a938379ec6912278177ee5756a35322c29818
Joel Robertson <joel@joelrobertson.net>
2022-03-04 17:19:04 +1300

    add cors configuration

---
+++ src/main/java/net/joelrobertson/pizza/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package net.joelrobertson.pizza;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNullApi;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration
+{
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200/");
+            }
+        };
+    }
+}
